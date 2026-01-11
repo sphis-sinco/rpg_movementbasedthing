c56856820349c67d0b1bac676567a32828598d9a
Viwu <sinconsistencia@gmail.com>
2026-01-10 18:55:02 -0600

    Testing tiles

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,23 @@
 package;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var tiles:FlxTypedGroup<Tile>;
+
 	override public function create()
 	{
 		super.create();
+
+		tiles = new FlxTypedGroup<Tile>();
+		add(tiles);
+
+		tiles.add(new Tile(0, Tile.DEFAULT_SIZE * 0));
+		tiles.add(new Tile(1, Tile.DEFAULT_SIZE * 1));
+		tiles.add(new Tile(2, Tile.DEFAULT_SIZE * 2));
+		tiles.add(new Tile(3, Tile.DEFAULT_SIZE * 3));
 	}
 
 	override public function update(elapsed:Float)
